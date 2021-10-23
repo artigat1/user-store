14c6385ca781dac3f3ae3e4dcbc467ba1e62de34
Steve Doggett <artigat1@gmail.com>
2021-10-23 10:41:27 +0100

    Initial commit

---
+++ src/components/user-create.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="row justify-content-center">
+        <div class="col-md-5">
+            <h3 class="text-center">
+                Add User
+            </h3>
+            <form @submit.prevent="onFormSubmit">
+                <div class="form-group">
+                    <label>Name</label>
+                    <input v-model="user.name"
+                           class="form-control"
+                           required
+                           type="text">
+                </div>
+
+                <div class="form-group">
+                    <label>Email</label>
+                    <input v-model="user.email"
+                           class="form-control"
+                           required
+                           type="email">
+                </div>
+
+                <div class="form-group">
+                    <label>Phone</label>
+                    <input v-model="user.phone"
+                           class="form-control"
+                           required
+                           type="text">
+                </div>
+
+                <div class="form-group">
+                    <button class="btn btn-primary btn-block">
+                        Add User
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+    
+    import { User } from "@/models/user";
+
+    interface  DataModel {
+        user: User,
+    } 
+    
+    export default Vue.extend({
+        data: (): DataModel => ({
+            user: {
+                name: '',
+                email: '',
+                phone: '',
+            },
+        }),
+        methods: {
+            onFormSubmit(): void {
+                alert(JSON.stringify(this.user))
+            },
+        },
+    })
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,29 +1,30 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    {
+        path: '/',
+        name: 'add',
+        component: () => import('../components/user-create.vue'),
+    },
+    {
+        path: '/list',
+        name: 'list',
+        component: () => import('../components/user-list.vue'),
+    },
+    {
+        path: '/edit/:id',
+        name: 'edit',
+        component: () => import('../components/user-edit.vue'),
+    },
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes,
 })
 
 export default router
